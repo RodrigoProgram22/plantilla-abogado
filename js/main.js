f3b42de25953e9ab385558815113f2164a30d2da
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2023-10-09 14:48:42 -0300

    Se mejoró el diseño del header

--- js/main.js
+++ js/main.js
@@ -8,3 +8,8 @@ function update() {
   }%`;
   requestAnimationFrame(update);
 }
+window.addEventListener("scroll", function () {
+  var nav = document.querySelector("nav");
+  nav.classList.remove("bg-transparent");
+  nav.classList.toggle("fondo-dark", window.scrollY > 0);
+});
